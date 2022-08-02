b16a51ff2ea540e08be90b4e50da7c791f229b0a
Shu Zhang <szhang@torstar.ca>
2022-08-02 13:27:54 -0400

    Completed module 4

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,11 +1,17 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById('logFilePath').value;
 
 // connection.addEventListener('open', () => {
 connection.onopen = () => {
-    connection.send('Hello from the client!');
+    if(filePath){
+        connection.send(filePath);
+    }else{
+        connection.send('Hello from the client!');
+    }
 }
 
 connection.onmessage = event => {
-    logWindow.innerHTML = event.data;
+    const logs = event.data.split("\n").join("<hr>");
+    logWindow.innerHTML = logs;
 }
