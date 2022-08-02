877f98db2f99bd1a2c1ffb2f4856214582d261bc
Shu Zhang <szhang@torstar.ca>
2022-08-02 17:14:39 -0400

    fix code format

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,34 +1,37 @@
-const fs = require('fs');
-const path = require('path');
-const settingsFilePath = path.join(__dirname, "../json/settings.json");
+const fs = require("fs")
+const path = require("path")
+
+const settingsFilePath = path.join(__dirname, "../json/settings.json")
 
 function getSettings() {
-  const settingsData = fs.readFileSync(settingsFilePath);
-  return JSON.parse(settingsData);
+  const settingsData = fs.readFileSync(settingsFilePath)
+  return JSON.parse(settingsData)
 }
 
 function writeSettings(newSettings) {
-  const settingsJSON = JSON.stringify(newSettings, null, 2);
-  try{
+  const settingsJSON = JSON.stringify(newSettings, null, 2)
+  try {
     fs.writeFileSync(settingsFilePath, settingsJSON);
-    return true;
-  }catch(err){
-    return false;
+    return true
+  } catch {
+    return false
   }
 }
 
 function getDefaultDir() {
-  const defaultDir = getSettings().defaultDir;
-  if(!defaultDir) return process.cwd();
-  return isValidDir(defaultDir) ? defaultDir : process.cwd();
+  const defaultDir = getSettings().defaultDir
+  if (!defaultDir) {
+    return process.cwd()
+  }
+  return isValidDir(defaultDir) ? defaultDir : process.cwd()
 }
 
 function isValidDir(dirPath) {
-  try{
-    fs.readdirSync(dirPath);
-    return true;
-  }catch(err){
-    return false;
+  try {
+    fs.readdirSync(dirPath)
+    return true
+  } catch {
+    return false
   }
 }
 
