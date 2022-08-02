868b5055eb743e595d312c356d58c928a2968d32
Shu Zhang <szhang@torstar.ca>
2022-08-02 14:35:41 -0400

    Completed module 5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,11 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+    const data = [];
+    files.forEach(file => {
+        if(isDirectory(currentDir, file)){
+            data.push({
+                name: file,
+                isDirectory: true,
+                path: path.join(query, file)
+            })
+        }else{
+            data.push({
+                name: file,
+                isDirectory: false,
+                path: path.join(query, file),
+                currentDir
+            })
+        }
+    })
+    return data
 }
 
 function isDirectory(currentDir, file) {
+    const fileInfor = fs.statSync(path.join(currentDir, file));
+    return fileInfor.isDirectory();
 }
 
 function readDir(currentDir, res, query) {
+    fs.readdir(currentDir, (err, files) => {
+        let directoryContents = [];
+        if(!err){
+            directoryContents = getDirectoryContents(files, currentDir, query);
+        }
+        res.json(directoryContents);
+    })
 }
 
 exports.get = (req, res) => {
+    let currentDir = dir;
+    const query = req.query.path ?? "";
+    if(query){
+        currentDir = fs.join(currentDir, query);
+    }
+    readDir(currentDir, res, query);
 };
